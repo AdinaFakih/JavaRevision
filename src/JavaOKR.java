548af6cc7d066e1b80b2423f9a8f4f613d5330b1
AdinaFakih <addy.fakih@gmail.com>
2021-10-02 18:17:17 +0500

    all

---
+++ src/JavaOKR.java
@@ -0,0 +1,163 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class JavaOKR {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("hii");
+//		System.out.println();
+//		
+//		int x = 5;
+//		double y = 8.4;
+//		boolean flag = true;
+//		
+//		System.out.println("x: " + x);
+//		System.out.println("y: " + y);
+//		System.out.println("flag: " + flag);
+//		
+//		/*
+//		 * comment
+//		 */
+//		
+//		String dept = "sqa";
+//		char letter = 'a';
+//		
+//		String course = dept + 500;
+//		String grade = letter + "";
+//		
+//		String courseInfo = course + ": " + grade;
+//		System.out.println(courseInfo);
+//		
+//		/*
+//		 * Math 
+//		 */
+//		double d = 2 * x + 10;
+//		double z = 2 * y + 5;
+//		System.out.println();
+//		System.out.println("d: " + d);
+//		System.out.println("z: " + z);
+//		
+//		//division wiyh ints returns remainder
+//		
+//		System.out.println("x/2:" + (x/2));
+//		System.out.println("x pow 4: " + Math.pow(x,4));
+//		
+//		System.out.println();
+//		System.out.println(x);
+//		
+//		if (x % 2 == 0) {
+//			System.out.println(x +"is even");
+//		} else {
+//			System.out.println(x + "is odd");
+//		}
+//		
+//		//cast int to String
+//		String intToString = Integer.toString(1);
+//		
+//		//cast double to String
+//		String doubleToString = Double.toString(1);
+//		
+//		System.out.println();
+//		System.out.println(intToString);
+//		System.out.println(doubleToString);
+//		
+//		//cast String to int
+//		//String StringToInt = Integer.parseInt("1");
+//		//String StringToDouble = Double.parseDouble("1");
+//		
+////		System.out.println(stringToDouble);
+////		System.out.println(((Object)stringToDouble).getClass());
+//		
+//		
+//		/*
+//		 * User Input
+//		 */
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println();
+//		System.out.println("Enter num");
+//		int myint = scan.nextInt();
+//		System.out.println(myint);
+//		
+//		//Array of objects initialing
+//		Customer[] cus;
+//		
+//		int[] prime = {2,3,5,7};
+//		
+//		//lenght of array
+//		int[] scores = new int[10];
+//		Customer[] cus = {new Customer("adi"), new Customer("adiiii")};
+		
+		//loop
+		String[] names = {"adina","addy", "adi"};
+		for (String n : names) {
+			System.out.println("Name:  " + n);		
+		}	
+		
+		System.out.println();
+		
+		for (int i = 0; i< names.length; i++) {
+			System.out.println("Name2:  " + names[i]);		
+		}	
+		
+		// Array of array
+		//array of 3 array, each of which points to an array of 2 ints 
+		int[][] table = new int[3][2];
+		
+		int count = 1;
+		for(int i = 0; i < table.length; i++) { //get the length of the row (vertical)
+			for(int j = 0; j < table.length; j++) { //get the length of the columns (horizontal)
+				table [i][j] = count++; //set count value in each array slot, then increment
+			}
+		}
+		
+		int [][] table2 = {
+				{1, 2},
+				{3,4},
+				{5,6}
+		};
+		
+		table2[2] = new int[20];
+		
+		//2 nested arrays, Array.deepEquals method to do comparison 
+		System.out.println(Arrays.deepEquals(table, table2));
+		
+		
+		//access the values
+		System.out.println(table[0][0]);
+		System.out.println(table[2][2]);
+		
+		// array list can only contain Objects and you can't store diff types in a single ArrayList
+		//ArrayList<Data Type> myArrayList = new ArrayList<Data Type>();
+		
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+		ArrayList<String> myArrayList1 = new ArrayList<String>();
+		ArrayList<Double> myArrayList2 = new ArrayList<Double>();
+		
+		scores.add(23);
+		scores.add(15);
+		scores.add(0);
+		
+		System.out.println(scores.size());
+		
+		//primitives
+		int a = 5;
+		System.out.println("a=" + a);
+		
+		boolean b = false;
+		System.out.println("b =" + b);
+		
+		//printing Objects
+		Customer c = new Customer("Brandon");
+		System.out.println("c = " + c);
+		
+		//u need to tell Java what to print by defining toString method
+		
+		public String toString() {
+			return "someString";
+		}
+		
+		
+		
+	}
+}
